4a06d6b49bfa977ec813eba97310c86af76208b9
RahmatHidayat03 <rahmathidayat22muhammad@gmail.com>
2024-07-30 13:36:34 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app" class="container mt-5">
+    <h1>IDShop</h1>
+    <product-list :products="products" :maximum="maximum"></product-list>
+    <!-- <font-awesome-icon icon="cart-shopping" />
+    <price-component :value="4.37"></price-component> -->
+  </div>
+</template>
+
+<script>
+// import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+// import PriceComponent from "./components/PriceComponent.vue";
+import ProductList from "./components/ProductList.vue";
+
+export default {
+  name: "App",
+  data: function () {
+    return {
+      maximum: 50,
+      products: [],
+    };
+  },
+
+  components: {
+    // FontAwesomeIcon,
+    // PriceComponent,
+    ProductList,
+  },
+  mounted: function () {
+    fetch("https://hplussport.com/api/products/order/price")
+      .then((response) => response.json())
+      .then((data) => {
+        this.products = data;
+      });
+  },
+};
+</script>
---
+++ src/components/ProductList.vue
@@ -0,0 +1,63 @@
+<template>
+  <transition-group
+    name="fade"
+    tag="div"
+    @before-enter="before"
+    @enter="enter"
+    @leave="leave"
+  >
+    <div
+      class="row d-none mb-3 align-items-center"
+      v-for="(item, index) in showItem"
+      :key="index :data-index=index">
+        <div class="col-1 m-auto">
+            <button class="btn btn-info" @click="$emit('add',item)">+</button>
+        </div>
+        <div class="col-sm-4">
+            <img :src="item.image" :alt="item.name" class="img-fluid d-block">
+        </div>
+        <div class="col">
+            <h3 class="text-info">{{ item.name }}</h3>
+            <p class="h5 float-right"></p>
+            <price-component  :value="Number(item.price)"></price-component>
+        </div>
+    </div>
+  </transition-group>
+</template>
+
+<script>
+import PriceComponent from './PriceComponent.vue';
+
+export default {
+    name: "product-list",
+    comments: {
+        PriceComponent,
+    },
+    props: ["product", "maximum"],
+    computed: {
+        showItem: function() {
+            let max = this.maximum;
+            return this.products.filter(function(item) {
+                return item.price <= max;
+            });
+        }
+    },
+    methods: {
+        before: function (el) {
+            el.className = 'd-none'
+        },
+        enter: function (el) {
+            var delay = el.dataset.index * 100;
+            setTimeout(function () {
+                el.className = 'row d-flex mb-3 align-items-center'
+            }, delay)
+        },
+        leave: function (el) {
+      var delay = el.dataset.index * 100;
+      setTimeout(function () {
+        el.className = "row d-flex mb-3 align-items-center ";
+            }, delay)
+        },
+    }
+}
+</script>
